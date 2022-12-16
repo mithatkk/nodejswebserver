fbba4c883224ce9f14528ae3f9b7e49ad4223b1d
mithatkk <mithat.kucuk@ozbeyfinans.com.tr>
2022-12-16 11:20:48 +0300

    dosyalar degistirildi

--- server-express.js
+++ server-express.js
@@ -1,13 +1,17 @@
-var express=require("express");
-var app=express();
-app.get("/",function(req,res){
-    res.send("sunucu çalışıyor!!!")
+var express = require("express");
+var app = express();
+var PORT = 3000;
 
+var middleware  = require("./middleware");
+
+app.use(middleware.logger);
+
+app.get("/hakkimda", middleware.requireAuthentication, function (req, res) {
+    res.send("Hakkımda sayfası!!!");
 })
-app.get("/anasayfa",function(req,res){
-res.send("sunucu çalışıyor");
-console.log("sunucu çalışıyor");
 
-})
-app.listen(8080);
+app.use(express.static(__dirname + '/public'));
 
+app.listen(PORT, function () {
+    console.log("Express server " + PORT + " nolu portta çalışıyor...");
+});
