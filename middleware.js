7155fe760d88b0acf01d853585b7c0087ad65d96
mithatkk <mithat.kucuk@ozbeyfinans.com.tr>
2022-12-16 11:08:32 +0300

    dosyalar eklenmiş ve düzenlenmiştir

---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports={
+    requireAuthentication: function (req, res, next) {
+        console.log("Özel route girildi!!!");
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log(req.method + " " + req.originalUrl);
+        next();
+    }
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+var middleware  = require("middleware");
+
+app.use(middleware.logger);
+
+app.get("/hakkimda", middleware.requireAuthentication, function (req, res) {
+    res.send("Hakkımda sayfası!!!");
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log("Express server " + PORT + " nolu portta çalışıyor...");
+});
